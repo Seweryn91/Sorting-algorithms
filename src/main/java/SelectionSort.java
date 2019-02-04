3906dc2aa0fefde32bd57b591e2c5a8a395b4ef8
Seweryn91 <s.talaj@gmail.com>
2019-02-04 13:28:09 +0100

    Implement method sort

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,2 +1,20 @@
 public class SelectionSort {
+
+    public void sort(int[] array) {
+        int len = array.length;
+
+        for (int i = 0; i < len-1; i++) {
+            int min_index = i;
+
+            for (int j = i+1; j < len; j++) {
+                if (array[j] < array[min_index]) {
+                    min_index = j;
+                }
+            }
+            int temp = array[min_index];
+            array[min_index] = array[i];
+            array[i] = temp;
+        }
+    }
+
 }
