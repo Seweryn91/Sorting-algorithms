cd64d265d80f46f7b6ee989435ebd58e3b21d2eb
Seweryn91 <s.talaj@gmail.com>
2019-02-04 13:42:17 +0100

    Create method testSort

--- src/test/java/SelectionSortTest.java
+++ src/test/java/SelectionSortTest.java
@@ -1,5 +1,23 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SelectionSortTest {
+
+    @Test
+    void testSort() {
+        int[] testArray = createArray();
+        int[] sortedArray = {0,1,2,3,4,5,6,7,8,9};
+        SelectionSort ss = new SelectionSort();
+        ss.sort(testArray);
+        String testArrayString = ss.arrayToString(testArray);
+        String sortedArrayString = ss.arrayToString(sortedArray);
+        assertEquals(sortedArrayString, testArrayString);
+    }
+
+    int[] createArray() {
+        int[] array = {8,7,5,3,6,1,2,0,4,9};
+        return array;
+    }
 
 }
