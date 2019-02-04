4cd9734f4e6a6176834b856e90a7435b14641606
Seweryn91 <s.talaj@gmail.com>
2019-02-04 13:29:11 +0100

    Implement method arrayToString

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -17,4 +17,18 @@ public class SelectionSort {
         }
     }
 
+    public String arrayToString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+
+        for (int i=0; i < array.length-1; ++i)
+            sb.append(array[i]+", ");
+
+        if (array.length > 0)
+            sb.append(array[array.length-1]);
+
+        sb.append(']');
+        return sb.toString();
+    }
+    
 }
